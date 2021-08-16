b93838a9a0dc8247ea79207a7c6bd821435b4d0e
samirboudaa <boudaa_samir@hotmail.fr>
2021-08-16 15:54:11 +0200

    add

---
+++ precache-manifest.2a843a7996685d5940200d21942852e5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59505a8f1686c4c973d5585f5858f586",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a91d5cff9ea5b86b369a",
+    "url": "/static/css/main.0ee0c3cf.chunk.css"
+  },
+  {
+    "revision": "d2bdff07a2f46f06a4e8",
+    "url": "/static/js/2.5e94ba83.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.5e94ba83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a91d5cff9ea5b86b369a",
+    "url": "/static/js/main.890168aa.chunk.js"
+  },
+  {
+    "revision": "dcc534e5adf1fce6f480",
+    "url": "/static/js/runtime-main.5f677427.js"
+  },
+  {
+    "revision": "6f82017092b3f62c335565b373a29482",
+    "url": "/static/media/KronaOne-Regular.6f820170.ttf"
+  },
+  {
+    "revision": "37c8a1aaa2c1891aa0960ff03c377649",
+    "url": "/static/media/Questrial-Regular.37c8a1aa.ttf"
+  }
+]);
